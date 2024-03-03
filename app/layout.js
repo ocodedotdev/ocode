40a71211fb80b631058f2b1facefbacbd5364784
soumya <mondalsoumya2001@gmail.com>
2024-03-03 14:58:34 +0530

    basic landing page

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,13 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Ocode.dev early access",
@@ -12,9 +17,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <ThemeProvider attribute="class" defaultTheme="system">
+          <Navbar />
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import {
   Tooltip,
@@ -6,20 +8,56 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+import { CalendarCheck2 } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Button variant="outline">Hover</Button>
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Add to library</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
+      <section className="flex flex-col gap-5 items-center px-10">
+        <h1 className="text-4xl md:text-5xl font-bold text-center leading-10">
+          Always Ahead with <span className="text-indigo-400">OCode</span>
+        </h1>
+        <p className="text-xs md:text-sm text-gray-700 dark:text-gray-400 text-center">
+          Discover exactly the tool you need to excel in development
+        </p>
+        <div className="flex gap-5">
+          <Button>Stay updated</Button>
+          <button
+            onClick={() =>
+              toast.custom(t =>
+                <div className="max-w-md w-full bg-white dark:bg-slate-900 text-black dark:text-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5">
+                  <div className="flex-1 w-0 p-4">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0 pt-0.5">
+                        <CalendarCheck2 className="w-6" />
+                      </div>
+                      <div className="ml-3 flex-1 font-bold">
+                        <p className="text-sm font-medium">
+                          Will soon be Available
+                        </p>
+                        <p className="mt-1 text-sm font-light">
+                          Stay updated via email!
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex border-l border-gray-200">
+                    <button
+                      onClick={() => toast.dismiss(t.id)}
+                      className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              )}
+            className="text-sm font-bold"
+          >
+            Try Demo
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
